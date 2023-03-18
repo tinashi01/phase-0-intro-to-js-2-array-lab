3f6e522cc34d50e8c56a8cd2f8552872b86dd8c1
Tina Shi <init.defaultBranch>
2023-03-18 17:27:30 +1100

    commit tree 55c869fe188abb1c53457d39064b6566459ba895

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendCat(name) {
+    return cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+}
+
+function appendCat(name = "Broom") {
+    const newCats = [...cats, name];
+    return newCats;
+}
+
+function prependCat(name = "Arnold") {
+    const newCats1 = [ name, ...cats];
+    return newCats1;
+}
+
+function removeLastCat() {
+    return cats.slice(0, -1);
+}
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
